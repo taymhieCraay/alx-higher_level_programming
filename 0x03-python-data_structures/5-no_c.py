c9fa794ea2a0188506888274ecd345b74e17a73c
taymhieCraay <ekundayotimi2018@gmail.com>
2022-12-03 12:55:54 -0800

    Write a function that removes all characters c and C from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if(my_list is not None):
-        m_list = my_list.copy()
-        if idx < 0 or idx >= len(my_list):
-            return my_list
-        else:
-            m_list[idx] = element
-            return m_list
+def no_c(my_string):
+    """Function that removes all c and C characters from a string"""
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_string += my_string[i]
+    return new_string
